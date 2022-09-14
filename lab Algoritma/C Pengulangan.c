b8682ec4f27a929022a7698635277d9d136005d1
salsaputri1 <113466627+salsaputri1@users.noreply.github.com>
2022-09-14 21:32:01 +0700

    Add files via upload

---
+++ lab Algoritma/C Pengulangan.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    for(int i=0; i<5; i++){
+        printf("Belajar Koding di Kelas Algoritma\n");
+    }
+    
+    return 0;
+}
---
+++ lab Algoritma/C++ Pengulangan.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 5; i++){
+        printf("Belajar Koding di Kelas Algoritma ke-%i\n", i);
+    }
+
+    return 0;
+}
